f4c05784ce707b2b969ae42be9a63f5598bdeb28
jaikrai <jaikrai0@gmail.com>
2021-08-30 15:15:46 -0400

    Tic tac teo game added

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,22 @@
 public class BankAccount {
-    protected String owner;
+    public static String owner;
     private double balance;
     public double getBalance(){ return 0;}
+
+    public String newOwner(){
+        return owner;
+    }
+    public static int sum(int a, int b){
+
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1; int b = Integer.parseInt(null);
+        if ( a == Integer.parseInt(null) || b == Integer.parseInt(null)){
+            throw new NumberFormatException("Null not excepted");
+        }
+        System.out.println(sum(a,b));
+    }
 
 }
